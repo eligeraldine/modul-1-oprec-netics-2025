99b8f49b02bb6a2741e8deb2ae65cad423d9fc87
eligeraldine <eli@LAPTOP-FH5E3QH3>
2025-04-06 18:16:54 +0700

    delete unnecessary code

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 import (
 	"net/http"
 	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 var startTime = time.Now()
 
 func main() {
-    router := gin.Default()
-    router.GET("/health", func(c*gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "nama":      "Pikachu",
-            "nrp":       "5025231021",
-            "status":    "UP",
-            "timestamp": time.Now().Format(time.RFC3339),
-            "uptime":    time.Since(startTime).String(),
-        })
-    })
-    router.Run("0.0.0.0:8080")
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"nama":      "Rouli Elizabeth Geraldine Aritonang",
+			"nrp":       "5025231021",
+			"status":    "UP",
+			"timestamp": time.Now().Format(time.RFC3339),
+			"uptime":    time.Since(startTime).Truncate(time.Second).String(),
+		})
+	})
+	router.Run("0.0.0.0:8080")
 }
